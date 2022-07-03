3caf7563c96be2a69e08116c902f8fae7974d8ff
frodrickfronkenstein <ericandrewsmith88@gmail.com>
2022-07-02 21:47:19 -0400

    crud features added

--- server.js
+++ server.js
@@ -1,4 +1,27 @@
+// const express = require('express');
+// const mongoose = require('mongoose');
+
+// const app = express();
+// const PORT = process.env.PORT || 3001;
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.static('public'));
+
+// app.use(require('./routes'));
+
+// mongoose.connect(process.env.MONGODB_URI || 'mongobd://localhost:27017/pizza-hunt', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// });
+
+// mongoose.set('debug', true);
+
+// app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
+
+
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -6,7 +29,14 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-
 app.use(require('./routes'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hunt', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true 
+}); 
+
+// Use this to log mongo queries being executed!
+mongoose.set('debug', true);
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
